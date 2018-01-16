e801ba08794ef99dbe0a7ecd32a1e32c11ca16dc
kccjay <cchave3462@greenvilleschools.us>
2018-01-16 09:15:05 -0500

    Add files via upload

---
+++ pygame.py
@@ -0,0 +1,80 @@
+# Imports
+import pygame
+import math
+
+# Initialize game engine
+pygame.init()
+
+
+# Window
+SIZE = (800, 600)
+TITLE = "My Awesome Picture"
+screen = pygame.display.set_mode(SIZE)
+pygame.display.set_caption(TITLE)
+
+
+# Timer
+clock = pygame.time.Clock()
+refresh_rate = 60
+
+
+# Colors
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+SKY = (0, 220, 248)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+ORANGE = (255, 125 , 0)
+TAN = (210, 125, 0)
+SUN = (255, 254, 0)
+    
+
+
+
+def draw_cloud(x, y):
+    pygame.draw.ellipse(screen, WHITE, [x, y + 20, 40 , 40])
+    pygame.draw.ellipse(screen, WHITE, [x + 60, y + 20, 40 , 40])
+    pygame.draw.ellipse(screen, WHITE, [x + 20, y + 10, 25, 25])
+    pygame.draw.ellipse(screen, WHITE, [x + 35, y, 50, 50])
+    pygame.draw.rect(screen, WHITE, [x + 20, y + 20, 60, 40])
+
+
+
+# Game loop
+done = False
+
+while not done:
+    # Event processing (React to key presses, mouse clicks, etc.)
+    ''' for now, we'll just check to see if the X is clicked '''
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+
+    # Game logic (Check for collisions, update points, etc.)
+    ''' leave this section alone for now ''' 
+
+    # Drawing code (Describe the picture. It isn't actually drawn yet.)
+    screen.fill(SKY)
+    pygame.draw.rect(screen, GREEN, [0, 500, 800, 500])
+    pygame.draw.polygon(screen, TAN, [[400, 280], [300,500], [500,500]])
+    pygame.draw.ellipse(screen, SUN, [800,0, 680, 120], 1)
+    pygame.draw.polygon(screen, BLACK, [[370, 500], [430, 440], [370, 440],[430, 500]])
+    
+    draw_cloud(50, 150)
+    draw_cloud(250, 75)
+    draw_cloud(350, 125)
+    draw_cloud(450, 175)
+    draw_cloud(650, 100)
+    
+    # Update screen (Actually draw the picture in the window.)
+    pygame.display.flip()
+
+
+    # Limit refresh rate of game loop 
+    clock.tick(refresh_rate)
+
+
+# Close window and quit
+pygame.quit()
